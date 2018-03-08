cdd8b3560701c4bb97a2bfd8e642bc8a66df8caa
Joseph alicea <Josephalicea96@gmail.com>
2018-03-08 14:20:26 -0500

    project #2
    Project #2

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+const
+    WIDTH = 500,
+    HEIGHT = WIDTH;
+
+function setup() {
+  createCanvas(640, 480);
+
+}
+function draw() {
+  ellipse(320, 180, 100, 100);
+  ellipse(320, 300, 100, 150);
+  triangle(180, 100, 100,100,120,120)
+}
